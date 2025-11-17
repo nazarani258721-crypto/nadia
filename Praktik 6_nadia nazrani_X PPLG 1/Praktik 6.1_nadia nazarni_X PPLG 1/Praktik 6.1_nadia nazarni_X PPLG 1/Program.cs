f6bd5f3666d5af6eb3ceb19ccc1a455036c8993e
nazarani258721-crypto <nazarani258721@student.smkn1kandeman.sch.id>
2025-11-17 13:39:11 +0700

    Add files via upload

---
+++ Praktik 6_nadia nazrani_X PPLG 1/Praktik 6.1_nadia nazarni_X PPLG 1/Praktik 6.1_nadia nazarni_X PPLG 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_6._1_nadia_nazarni_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat array dengan 5 elemen tipe data string
+            string[] siswa = new string[5];
+
+            //Mengisi elemen array
+            siswa[0] = "Andi";
+            siswa[1] = "Budi";
+            siswa[2] = "vano";
+            siswa[3] = "adit";
+            siswa[4] = "bambang";
+
+            //Menampilkan isi array tanpa perulangan
+            Console.WriteLine("Daftar Nama siswa:");
+            Console.WriteLine("Siswa Ke-1: " + siswa[0]);
+            Console.WriteLine("Siswa Ke-2: " + siswa[1]);
+            Console.WriteLine("Siswa Ke-3: " + siswa[2]);
+            Console.WriteLine("Siswa Ke-4: " + siswa[3]);
+            Console.WriteLine("Siswa Ke-5: " + siswa[4]);
+        }
+    }
+}
---
+++ Praktik 6_nadia nazrani_X PPLG 1/Praktik 6.2_nadia nazarani_X PPLG 1/Praktik 6.2_nadia nazarani_X PPLG 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_6._2_nadia_nazarani_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat array integer dengan keyboard
+            int[] angka = new int[5];
+
+            //input elemen array dari keyboard
+            Console.Write("masukkan angka -1 :");
+            angka[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("masukkan angka -2 :");
+            angka[1] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("masukkan angka -3 :");
+            angka[2] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("masukkan angka -4 :");
+            angka[3] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("masukkan angka -5 :");
+            angka[4] = Convert.ToInt32(Console.ReadLine());
+
+            //Menampilkan isi array tanpa perulangan
+            Console.WriteLine("\nData yang anda masukkan:");
+            Console.WriteLine("Nilai ke-1: " + angka[0]);
+            Console.WriteLine("Nilai ke-2: " + angka[1]);
+            Console.WriteLine("Nilai ke-3: " + angka[2]);
+            Console.WriteLine("Nilai ke-4: " + angka[3]);
+            Console.WriteLine("Nilai ke-5: " + angka[4]);
+        }
+    }
+}
