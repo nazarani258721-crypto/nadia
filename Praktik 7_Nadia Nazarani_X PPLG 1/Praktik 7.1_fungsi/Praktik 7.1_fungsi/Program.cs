ad7599776075b1fb08108451b9c1a9e1af52376b
nazarani258721-crypto <nazarani258721@student.smkn1kandeman.sch.id>
2025-11-18 12:24:11 +0700

    Add files via upload

---
+++ Praktik 7_Nadia Nazarani_X PPLG 1/Praktik 7.1_fungsi/Praktik 7.1_fungsi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_7._1_fungsi
+{
+    internal class Program
+    {
+        static void Sapa()
+        {
+            Console.WriteLine("Halo, selamat datang di pembelajaran C#");
+            Console.WriteLine("Mari belajar tentanng fungsi!");
+        }
+        static void Main(string[] args)
+        {
+            Sapa(); // Memangil fungsi
+        }
+    }
+}
